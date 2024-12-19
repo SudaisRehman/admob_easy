67e7f3ef282d390aed89d784ef4904a6c06104fc
SudaisRehman <114279778+SudaisRehman@users.noreply.github.com>
2024-12-19 21:10:16 +0500

    Update admob_easy_native.dart

--- lib/ads/services/admob_easy_native.dart
+++ lib/ads/services/admob_easy_native.dart
@@ -3,6 +3,39 @@ import 'package:flutter/material.dart';
 
 /// A widget that displays a native ad from AdMob.
 class AdmobEasyNative extends StatefulWidget {
+  /// The minimum width of the ad.
+  final double minWidth;
+
+  /// The minimum height of the ad.
+  final double minHeight;
+
+  /// The maximum width of the ad.
+  final double maxWidth;
+
+  /// The maximum height of the ad.
+  final double maxHeight;
+
+  /// Called when the ad is clicked.
+  final void Function(Ad)? onAdClicked;
+
+  /// Called when the ad is impression.
+  final void Function(Ad)? onAdImpression;
+
+  /// Called when the ad is closed.
+  final void Function(Ad)? onAdClosed;
+
+  /// Called when the ad is opened.
+  final void Function(Ad)? onAdOpened;
+
+  /// Called when the ad will dismiss the screen.
+  final void Function(Ad)? onAdWillDismissScreen;
+
+  /// Called when the ad receives a paid event.
+  final void Function(Ad, double, PrecisionType, String)? onPaidEvent;
+
+  final TemplateType templateType;
+
+  /// A small template for the native ad.
   const AdmobEasyNative.smallTemplate({
     this.minWidth = 320,
     this.minHeight = 90,
@@ -18,46 +51,63 @@ class AdmobEasyNative extends StatefulWidget {
     this.onPaidEvent,
   });
 
-  final double minWidth;
-  final double minHeight;
-  final double maxWidth;
-  final double maxHeight;
-  final TemplateType templateType;
-  final void Function(Ad)? onAdClicked;
-  final void Function(Ad)? onAdClosed;
-  final void Function(Ad)? onAdImpression;
-  final void Function(Ad)? onAdOpened;
-  final void Function(Ad)? onAdWillDismissScreen;
-  final void Function(Ad, double, PrecisionType, String)? onPaidEvent;
+  /// A medium template for the native ad.
+  const AdmobEasyNative.mediumTemplate({
+    this.minWidth = 320,
+    this.minHeight = 320,
+    this.maxWidth = 400,
+    this.maxHeight = 400,
+    this.templateType = TemplateType.medium,
+    super.key,
+    this.onAdClicked,
+    this.onAdClosed,
+    this.onAdImpression,
+    this.onAdOpened,
+    this.onAdWillDismissScreen,
+    this.onPaidEvent,
+  });
 
   @override
   State<AdmobEasyNative> createState() => _AdmobEasyNativeState();
 }
 
 class _AdmobEasyNativeState extends State<AdmobEasyNative> {
-  NativeAd? _nativeAd;
-  bool _isAdLoaded = false;
+  final _nativeAd = ValueNotifier<NativeAd?>(null);
+  final _nativeAdIsLoaded = ValueNotifier<bool>(false);
 
-  @override
-  void initState() {
-    super.initState();
-    _loadNativeAd();
+  /// Initializes the native ad.
+  Future<void> _init() async {
+    if (!AdmobEasy.instance.isConnected.value ||
+        AdmobEasy.instance.nativeAdID.isEmpty) {
+      AdmobEasyLogger.error('Admob not connected or ad unit ID is empty');
+      _nativeAdIsLoaded.value = false;
+      return;
+    }
+
+    _loadAd();
   }
 
-  void _loadNativeAd() {
+  /// Loads a native ad.
+  void _loadAd() {
     final ad = NativeAd(
       adUnitId: AdmobEasy.instance.nativeAdID,
       listener: NativeAdListener(
         onAdLoaded: (ad) {
-          setState(() {
-            _nativeAd = ad as NativeAd;
-            _isAdLoaded = true; // Ad is loaded
-          });
+          AdmobEasyLogger.success('NativeAd loaded.');
+          _nativeAd.value = ad as NativeAd;
+          _nativeAdIsLoaded.value = true;
         },
         onAdFailedToLoad: (ad, error) {
+          AdmobEasyLogger.error('NativeAd failedToLoad: $error');
           ad.dispose();
-          print('NativeAd failed to load: $error');
+          _nativeAdIsLoaded.value = false;
         },
+        onAdClicked: widget.onAdClicked,
+        onAdImpression: widget.onAdImpression,
+        onAdClosed: widget.onAdClosed,
+        onAdOpened: widget.onAdOpened,
+        onAdWillDismissScreen: widget.onAdWillDismissScreen,
+        onPaidEvent: widget.onPaidEvent,
       ),
       request: const AdRequest(),
       nativeTemplateStyle: NativeTemplateStyle(
@@ -69,32 +119,44 @@ class _AdmobEasyNativeState extends State<AdmobEasyNative> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  @override
   void dispose() {
-    _nativeAd?.dispose();
+    _nativeAd.value?.dispose();
+    _nativeAdIsLoaded.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!_isAdLoaded) {
-      return SizedBox(
-        width: widget.minWidth,
-        height: widget.minHeight,
-        child: Center(child: CircularProgressIndicator()),
-      );
-    }
+    return ValueListenableBuilder<bool>(
+      valueListenable: _nativeAdIsLoaded,
+      builder: (context, isAdLoaded, child) {
+        if (!isAdLoaded || _nativeAd.value == null) {
+          // Return an empty SizedBox when the ad is not loaded
+          return SizedBox(
+            width: widget.minWidth,
+            height: widget.minHeight,
+          );
+        }
 
-    return ConstrainedBox(
-      constraints: BoxConstraints(
-        minWidth: widget.minWidth,
-        minHeight: widget.minHeight,
-        maxWidth: widget.maxWidth,
-        maxHeight: widget.maxHeight,
-      ),
-      child: AdWidget(
-        ad: _nativeAd!,
-        key: ValueKey(_nativeAd!.hashCode),
-      ),
+        return ConstrainedBox(
+          constraints: BoxConstraints(
+            minWidth: widget.minWidth,
+            minHeight: widget.minHeight,
+            maxWidth: widget.maxWidth,
+            maxHeight: widget.maxHeight,
+          ),
+          child: AdWidget(
+            ad: _nativeAd.value!,
+            key: ValueKey(_nativeAd.value!.hashCode),
+          ),
+        );
+      },
     );
   }
 }
